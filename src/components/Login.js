6255f72138902190afb5b31666191a379e7bef4f
efleming111 <efleming111@yahoo.com>
2019-02-21 20:32:58 -0700

    Added login by google

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,31 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
 
 class Login extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            isAuth: false,
+            testReturn: ''
+        }
+    }
 
     handleLogin = event=>{
         event.preventDefault();
-        this.props.history.push('/notes');
+        axios.get('https://ericsnotesbackend.herokuapp.com/auth/google')
+        .then(data=>{
+            console.log(data.data.message);
+        })
+        .catch(error=>{
+            console.log('Something went wrong');
+        })
     }
 
     render(){
         return(
             <div onClick={this.handleLogin}>
-                Click Here To Login
+                Login in with Google+
             </div>
         )
     }
